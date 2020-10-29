696e696bbd20868fdafb88f96678856452b2a94f
Raymond <raymond.may.95@outlook.com>
2020-10-29 07:08:31 -1000

    just added to cart from local storage

--- storage-project.js
+++ storage-project.js
@@ -3,6 +3,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const itemValues = document.getElementById('items')
   const quantityValues = document.getElementById('quantity')
 
+  const renderCart = () => {
+    const items = Object.keys(localStorage)
+    const amount = Object.values(localStorage)
+    console.log(items, amount)
+    const unorderedList = document.createElement('ul')
+    unorderedList.setAttribute('id', 'unorderedList')
+    items.forEach((key, index)=>{
+      const liElement = document.createElement('li')
+      liElement.innerText = `${key}     quantity :${amount[index]}`
+      unorderedList.appendChild(liElement)
+
+    })
+    document.body.appendChild(unorderedList)
+  }
+  renderCart()
+
   addToCartButton.addEventListener('click', event => {
     localStorage.setItem(itemValues.value, quantityValues.value)
 
