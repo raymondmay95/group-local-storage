d4d1485e5a665e86b653b290288b7f4a6afab731
Raymond <raymond.may.95@outlook.com>
2020-10-29 06:43:45 -1000

    up too add storage test

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,12 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  const addToCartButton = document.getElementById('add-to-cart')
+  const itemValues = document.getElementById('items')
+  const quantityValues = document.getElementById('quantity')
 
-  // const showCart = () => {
+  addToCartButton.addEventListener('click', event => {
+    localStorage.setItem(itemValues.value, quantityValues.value)
 
-  // };
+  })
 
-  // const storeItem = () => {
-
-  // };
-
-  // const removeItem = () => {
-
-  // };
 
 });
